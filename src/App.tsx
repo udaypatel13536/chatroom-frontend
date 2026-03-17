3a4d9acfc661199091b39677094b7b79457d1f7f
Uday Patel <pateluday9725@gmail.com>
2026-03-17 16:56:35 +0530

    ready for bacis chatapp

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,23 @@ function App() {
  const [socket,setSocket] = useState<WebSocket | null>(null)
   const [messages,setMessage]= useState<messagesType[]>([])
   const [modelopen,setmodelOpen] = useState(true)
+  const [username , setUsarname] = useState("")
   const messageInputRef = useRef<HTMLInputElement>(null)
+  const usernameRef= useRef<HTMLInputElement>(null)
   useEffect(()=>{
-
-
-
     const ws = new WebSocket ("ws://localhost:8080")
       setSocket(ws)
     ws.onmessage= (e)=>{
       const data :messagesType =JSON.parse(e.data)
       setMessage((prev)=>[...prev ,data])
     }
+    return()=>{
+      ws.close()
+    }
+    
   },[])
   function send(){
-    const username:string = "Browser"
+    
     const message = messageInputRef.current?.value
     if(message && socket){
       socket.send(
@@ -33,6 +36,15 @@ function App() {
         })
       )
     }
+
+    messageInputRef.current!.value=  ""
+    
+  }
+  function Jion(){
+    const username = usernameRef.current?.value
+    setUsarname(username!)
+    setmodelOpen(false)
+
   }
 
   return (
@@ -40,12 +52,13 @@ function App() {
 <div className=' bg-neutral-900 w-screen h-screen'>
   {modelopen? <div className=' bg-neutral-900 fixed top-0 left-0 flex justify-center items-center w-screen h-screen'>
     <div className=' bg-gray-400 p-6 w-md h-80 flex flex-col justify-center  gap-15'>
-      <input  className="bg-gray-100 text-black  px-6"type='text' placeholder='Name'></input>
-      <button className='bg-gray-900 rounded-lg p-2 text-gray-200' onClick={()=>setmodelOpen(false)} >Join</button>
+      <input ref={usernameRef} className="bg-gray-100 text-black  px-6"type='text' placeholder='Name'></input>
+      <button className='bg-gray-900 rounded-lg p-2 text-gray-200' onClick={Jion} >Join</button>
 
     </div>
   </div>
   :<div  className='flex flex-col gap-4  justify-between items-center w-screen h-screen '>
+    <h1 className='text-3xl fornt-extrabold text-gray-200 '>{username}</h1>
     <div className=' mt-10 border w-sm h-8/10 rounded-xl p-6 text-gray-300'>
       {messages?.length !== 0 ? messages.map((msg,index)=>(
         <p key = {index}>{msg.username} :{msg.message}</p>
